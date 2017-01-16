JACOBIAN: Fix inncorrect jacobian

kf.py
@@ -5,7 +5,7 @@ import numpy as np
 import rospy
 import tf
 
-from geometry_msgs.msg import PoseStamped, Quaternion
+from geometry_msgs.msg import PoseStamped, Quaternion, Twist
 from sensor_msgs.msg import Imu
 
 
@@ -34,10 +34,8 @@ class KalmanFilter(object):
         '''
         u = np.array(u).reshape(-1, 1)
         assert Q.shape == (self._dims, self._dims), Q.shape
-        print "x[2] = ", self.x[2]
         self.x, Jf = self.F(self.x, u, dt)
         self.P = Jf.dot(self.P).dot(Jf.T) + Q
-        print "x'[2] = ", self.x[2]
 
     def correct(self, z, R):
         '''
@@ -64,7 +62,7 @@ class Aquire(object):
         self.last_imu = None
 
         rospy.Subscriber("/imu", Imu, self.got_imu)
-        rospy.Subscriber("/pos_sensor", PoseStamped, self.got_sensor)
+        rospy.Subscriber("/sensor", Twist, self.got_sensor)
 
     def got_imu(self, msg):
         '''
@@ -86,6 +84,7 @@ class Aquire(object):
         self.kf.predict(u, np.diag(Q), dt)
         self.imu_data = msg
         self.last_imu = msg
+        self.pub_esitmate()
 
     def got_sensor(self, msg):
         '''
@@ -94,14 +93,16 @@ class Aquire(object):
         if self.imu_data is None:
             return
         
-        x = msg.pose.position.x
-        y = msg.pose.position.y
+        x = msg.linear.x
+        y = msg.linear.y
         q = self.imu_data.orientation
         theta = tf.transformations.euler_from_quaternion([q.x, q.y, q.z, q.w])[2]
 
         R = np.diag([1E-4, 1E-4, self.imu_data.orientation_covariance[8]])
 
         self.kf.correct([x, y, theta], R)
+        self.imu_data = None
+
         self.pub_esitmate()
     
     def pub_esitmate(self):
@@ -118,7 +119,7 @@ class Aquire(object):
         p.pose.orientation = Quaternion(*tf.transformations.quaternion_from_euler(0, 0, theta))
 
         print "x:", np.round(self.kf.x.T, 2)
-        print "P:", np.round(self.kf.P.diagonal(), 2)
+        print "P:", np.round(self.kf.P.diagonal(), 4)
         print
         self.pub_est.publish(p)
 
@@ -143,12 +144,18 @@ if __name__ == "__main__":
         new_x[4] += (-s * u[0] + c * u[1]) * dt
         new_x[5] = u[2]
 
-        Jf = np.array([[1, 0, 0, c * dt, -s * dt,  0],
-                       [0, 1, 0, s * dt,  c * dt,  0],
-                       [0, 0, 1,      0,       0, dt],
-                       [0, 0, 0,      1,       0,  0],
-                       [0, 0, 0,      0,       1,  0],
-                       [0, 0, 0,      0,       0,  1]])
+        # Linearize around the previous esitmate (I think this is right)
+        c, s = np.cos(x[2]), np.sin(x[2])
+        dx0_dx2 = -s * x[3] * dt - c * x[4] * dt
+        dx1_dx2 = -c * x[3] * dt - s * x[4] * dt
+        dx3_dx2 = -s * u[0] * dt + c * u[1] * dt
+        dx4_dx2 = -c * u[0] * dt - s * u[1] * dt 
+        Jf = np.array([[1, 0, dx0_dx2, c * dt, -s * dt,  0],
+                       [0, 1, dx1_dx2, s * dt,  c * dt,  0],
+                       [0, 0,       1,      0,       0, dt],
+                       [0, 0, dx3_dx2,      1,       0,  0],
+                       [0, 0, dx4_dx2,      0,       1,  0],
+                       [0, 0,       0,      0,       0,  0]])
 
         return new_x, Jf
     
@@ -157,13 +164,13 @@ if __name__ == "__main__":
         # Return the error between the expected obs (H(x)) and the actual observation (z)
         # z := [x0, x1, theta]
         expected_z = np.zeros(3)
-        expected_z[0] = x[0]
-        expected_z[1] = x[1]
+        expected_z[0] = x[3]
+        expected_z[1] = x[4]
         expected_z[2] = x[2]
         
         # Jacobian of H function
-        Jh = np.array([[1, 0, 0, 0, 0, 0],
-                       [0, 1, 0, 0, 0, 0],
+        Jh = np.array([[0, 0, 0, 1, 0, 0],
+                       [0, 0, 0, 0, 1, 0],
                        [0, 0, 1, 0, 0, 0]])
 
         # Error
@@ -172,12 +179,12 @@ if __name__ == "__main__":
         # Stupid orientation - compute that error here
         cx, sx = np.cos(x[2]), np.sin(x[2])
         cz, sz = np.cos(z[2]), np.sin(z[2])
-        y[2] = np.arctan2(sz * cx - cz * sx, cz * cx + sz * sx )
+        y[2] = np.arctan2(sz * cx - cz * sx, cz * cx + sz * sx)
 
         return y.reshape(-1, 1), Jh
 
-    x0 = np.array([0, 0, 0, 0, 0, .9])
-    P0 = np.diag([1, 1, 1, 1, 1, 1E-5])
+    x0 = np.array([0, 0, 0, 0, 0, 0])
+    P0 = np.diag([1, 1, 1, 1E-3, 1, 1])
      
     kf = KalmanFilter(F, H, x0, P0, num_dims, num_sensors)
 