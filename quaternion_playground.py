QUATERNION: Fix dq function to not use approxmiates

quaternion_playground.py
@@ -10,33 +10,28 @@ from geometry_msgs.msg import Quaternion, PoseStamped
 def normalize(q):
     return q / np.linalg.norm(q)
 
-def get_dq(q, w):
-    '''
-    q := Quaternion with components [qx, qy, qz, qw] (standard tf convention)
-    w := Angular velocity [wx, wy, wz]  # rad/s
-    '''
-    dq = np.zeros(4).astype(np.float64)
-    dq[3] = 1/2 * (-(w[0] * q[0]) - (w[1] * q[1]) - (w[2] * q[2]))
-    dq[0] = 1/2 * ( (w[0] * q[3]) - (w[1] * q[2]) + (w[2] * q[1]))
-    dq[1] = 1/2 * ( (w[0] * q[2]) + (w[1] * q[3]) - (w[2] * q[0]))
-    dq[2] = 1/2 * (-(w[0] * q[1]) + (w[1] * q[0]) + (w[2] * q[3]))
-    return dq
+def get_dq(v):
+    r = np.zeros(4).astype(np.float64)
+    angle = np.linalg.norm(v)
+    r[3] = np.cos(angle / 2)
+    r[:3] = np.array(v) * np.sin(angle / 2) / angle  # sinc(angle / 2) / 2
+    return r
 
 if __name__ == "__main__":
     rospy.init_node("q_playground")
     pub = rospy.Publisher("/pose", PoseStamped, queue_size=1)
 
-    q0 = trns.quaternion_from_euler(0, 0, 0)
-    w = [0, 0, .01]  # x, y, z angular velocities (rad/s ?)
-    # This seems to represent rads when the numbers are suffienctly small
+    q0 = trns.quaternion_from_euler(0.9691, -0.76813, -1.83213)
+    rot = trns.quaternion_matrix(q0)[:3, :3]
+    w = [0, 0.1, 0]  # x, y, z angular velocities (rad/s ?)
 
     i = 0
     while not rospy.is_shutdown():
         # q' = q0 + dq * dt
-        dq = get_dq(q0, w)
-        q0 += dq
+        dq = get_dq(w)
+        q0 = trns.quaternion_multiply(q0, dq)
 
-        q1 = q0#normalize(q0)
+        q1 = normalize(q0)
         print i, "======================="
         print q1
         print np.round(trns.euler_from_quaternion(q1), 5)
@@ -48,5 +43,5 @@ if __name__ == "__main__":
         pose.header.frame_id = "map"
         pose.pose.orientation = Quaternion(*q1)
         pub.publish(pose)
-        rospy.sleep(.05)
+        rospy.sleep(.1)
 